e1c0ecf89c720bbe216d76348307be9ef9a30905
Sergey Khlynovskiy <khlynovs@ualberta.ca>
2026-05-06 17:20:59 -0600

    0 is now gripper open

--- libgripper/examples/test_gecko_gripper.cpp
+++ libgripper/examples/test_gecko_gripper.cpp
@@ -16,7 +16,7 @@ int main(int argc, char** argv) {
 
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
-    gripper.setPosition(-0.2);
+    gripper.setPosition(0.1);
     for (int i = 0; i < 1000; ++i) {
         // gripper.updateLocalState();
         gripper.controlLoopCallback();
